fbce8dd8b8e1a5d74047005edaf91f1ee12ea290
TrevinoRizkySunarja <trevprive123@gmail.com>
2024-01-10 12:26:36 +0100

    Version 1.4

--- index.php
+++ index.php
@@ -1,17 +1,41 @@
-<?php
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
 </head>
 <body>
-    test
+
+<?php
+// Controleer of het formulier is ingediend
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Controleer of de gebruikersnaam en het wachtwoord overeenkomen
+    $gebruikersnaam = "gebruiker";
+    $wachtwoord = "wachtwoord";
+
+    if ($_POST["gebruikersnaam"] == $gebruikersnaam && $_POST["wachtwoord"] == $wachtwoord) {
+        echo "<p>Welkom, $gebruikersnaam!</p>";
+    } else {
+        echo "<p>Ongeldige gebruikersnaam of wachtwoord.</p>";
+    }
+}
+?>
+
+<h2>Login</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <label for="gebruikersnaam">Gebruikersnaam:</label>
+    <input type="text" id="gebruikersnaam" name="gebruikersnaam" required>
+
+    <br>
+
+    <label for="wachtwoord">Wachtwoord:</label>
+    <input type="password" id="wachtwoord" name="wachtwoord" required>
+
+    <br>
+
+    <input type="submit" value="Login">
+</form>
+
 </body>
 </html>
-
