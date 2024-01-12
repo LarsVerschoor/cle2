4ac94da7baa77f50e65a38a199a969c49aa1e6cc
LarsVerschoor <larsverschoor2@gmail.com>
2024-01-12 10:16:58 +0100

    bugfix database.sql
    Added use kiryan_bv and added primary key to admins.id

--- includes/database.sql
+++ includes/database.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE kiryan_bv;
 
+USE kiryan_bv;
+
 CREATE TABLE admins (
-                        id BIGINT UNSIGNED AUTO_INCREMENT,
+                        id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                         username VARCHAR(30) UNIQUE,
                         password VARCHAR(255)
 );
