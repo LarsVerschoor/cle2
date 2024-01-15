e8b05dca0d36c91873e644de995c0f48e886d4fa
LarsVerschoor <larsverschoor2@gmail.com>
2024-01-15 09:53:40 +0100

    Updated database.sql

--- includes/database.sql
+++ includes/database.sql
@@ -1,11 +1,12 @@
+DROP DATABASE kiryan_bv;
 CREATE DATABASE kiryan_bv;
 
 USE kiryan_bv;
 
 CREATE TABLE admins (
-                        id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                        username VARCHAR(30) UNIQUE,
-                        password VARCHAR(255)
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) UNIQUE,
+    password VARCHAR(255)
 );
 
 INSERT INTO admins (username, password) VALUES ('admin', '$2y$10$AMux3z.fgceF2zQualXc0..AdVsXo6Q8LjwQBklc6JAPfUtsLqfhO');
