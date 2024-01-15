e5b83f01a77c8fdc96abfefd29554e1dd44c9713
LarsVerschoor <larsverschoor2@gmail.com>
2024-01-15 14:36:56 +0100

    Added materials & categories to database.sql

--- includes/database.sql
+++ includes/database.sql
@@ -18,15 +18,18 @@ CREATE TABLE customers (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE products (
+CREATE TABLE materials (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
+    name VARCHAR(100),
     description text,
-    image VARCHAR(255),
-    price DECIMAL(10, 2),
-    stock MEDIUMINT,
-    available BOOLEAN,
-    added_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    added DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categories (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    description text,
+    added DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE pickup_dates (
@@ -34,12 +37,28 @@ CREATE TABLE pickup_dates (
     date DATE DEFAULT CURRENT_DATE
 );
 
+CREATE TABLE products (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description text,
+    image VARCHAR(255),
+    price DECIMAL(10, 2),
+    material_id BIGINT UNSIGNED,
+    category_id BIGINT UNSIGNED,
+    stock MEDIUMINT,
+    available BOOLEAN,
+    added_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (material_id) REFERENCES materials(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
 CREATE TABLE reservations (
-  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  customer_id BIGINT UNSIGNED,
-  pickup_date_id BIGINT UNSIGNED,
-  FOREIGN KEY (customer_id) REFERENCES customers(id),
-  FOREIGN KEY (pickup_date_id) REFERENCES pickup_dates(id)
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    customer_id BIGINT UNSIGNED,
+    pickup_date_id BIGINT UNSIGNED,
+    pickup_completed BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (pickup_date_id) REFERENCES pickup_dates(id)
 );
 
 CREATE TABLE shopping_cart_items (
