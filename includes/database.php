7b6dc15dfa936e73afc83c1affc4ee2e4bae13f7
LarsVerschoor <larsverschoor2@gmail.com>
2024-01-16 10:05:09 +0100

    test push

--- includes/database.php
+++ includes/database.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $database = 'kiryan_bv';
 $user = 'root';
-$password = '';
+$password = 'Faxrol01';
 
 $db = mysqli_connect($host, $user, $password, $database)
     or die('Error: ' . mysqli_connect_error());
