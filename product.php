acf1339e993780d91bd24d69a4d752b828914190
Niko-lapd <54631340+Niko-lapd@users.noreply.github.com>
2024-01-16 10:37:36 +0100

    products pagina toegevoegd

---
+++ product.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/productcss.css">
+    <title>Products | Kiryan B.V.</title>
+</head>
+<body>
+<nav class="navbar">
+    <a href="">link</a>
+    <a href="">link</a>
+    <a href="signup-customer.php">Klant registreren</a>
+    <a href="login-customer.php">klant login</a>
+    <a href="login-admin.php">Admin login</a>
+</nav>
+<header>
+</header>
+<main>
+    <div class="product">
+        <h1>wand tegel Acuarella</h1>
+
+        <img src="images/wandtegelacurela.jpg" alt="Product Image">
+
+        <div class="product-info">
+            <p>Omschrijving: formaat 10 x 40 x 1 cm, 3 M² per pak, 15 kg per M², kleur beige, vorm rechthoek, oppervlak glanzend,</p>
+
+            <p>Prijs: €16,25 per M²</p>
+
+
+            <p>
+                <label for="quantity">Hoeveelheid</label>
+                <input type="number" id="quantity" name="quantity" min="1" value="1">
+            </p>
+
+            <button>Toevoegen aan winkelwagen</button>
+        </div>
+    </div>
+
+</main>
+<footer>
+    Kiyran B.V.
+    Bloklandweg 1A
+    4171 KA Herwijnen
+    info@kiryanbv.nl
+</footer>
+</body>
+</html>
